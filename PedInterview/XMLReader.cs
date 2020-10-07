32da1dc8b3294b8ecac619422ae870aa6d9250e1
Rich Dunne <richdunne@yahoo.com>
2020-10-07 05:13:09 -0600

    Refactored to use XElements instead of just their values

--- PedInterview/XMLReader.cs
+++ PedInterview/XMLReader.cs
@@ -7,9 +7,11 @@ namespace PedInterview
 {
     internal class XMLReader
     {
-        internal static Dictionary<string, Dictionary<string, List<string>>> ReadXML(string filename)
+        internal static Dictionary<string, Dictionary<XElement, List<XElement>>> ReadXML(string filename)
+        //internal static Dictionary<string, Dictionary<string, List<string>>> ReadXML(string filename)
         {
-            var questionsAndAnswers = new Dictionary<string, Dictionary<string, List<string>>>();
+            var questionsAndAnswers = new Dictionary<string,Dictionary<XElement, List<XElement>>>();
+            //var questionsAndAnswers = new Dictionary<string, Dictionary<string, List<string>>>();
             var currentDirectory = Directory.GetCurrentDirectory() + "\\plugins";
             var xmlFilepath = Path.Combine(currentDirectory, filename);
 
@@ -20,17 +22,21 @@ namespace PedInterview
 
             foreach (XAttribute category in questionCategories)
             {
-                var localQuestions = new Dictionary<string, List<string>>();
+                var localQuestions = new Dictionary<XElement, List<XElement>>();
+                //var localQuestions = new Dictionary<string, List<string>>();
                 foreach (XElement question in questions.Where(x => x.Parent == category.Parent))
                 {
                     //Game.LogTrivial($"Question: {question.Attribute("question").Value}");
-                    var localResponses = new List<string>();
+                    var localResponses = new List<XElement>();
+                    //var localResponses = new List<string>();
                     foreach (XElement response in responses.Where(r => r.Parent.Attribute("question").Value == question.Attribute("question").Value))
                     {
-                        localResponses.Add(response.Value);
+                        localResponses.Add(response);
+                        //localResponses.Add(response.Value);
                         //Game.LogTrivial($"Response: {response.Value}");
                     }
-                    localQuestions.Add(question.Attribute("question").Value, localResponses);
+                    localQuestions.Add(question, localResponses);
+                    //localQuestions.Add(question.Attribute("question").Value, localResponses);
                 }
                 questionsAndAnswers.Add(category.Value, localQuestions);
             }
