8cd4dd92cd726690c40e6130e09940dcdde193d7
Rich Dunne <richdunne@yahoo.com>
2020-12-27 10:32:22 -0700

    Overhaul with new XML structure

--- BetterPedInteractions/XMLReader.cs
+++ BetterPedInteractions/XMLReader.cs
@@ -1,4 +1,5 @@
 ﻿using Rage;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,66 +11,99 @@ namespace BetterPedInteractions
     {
         internal static void ReadXMLs()
         {
-            var questionsAndAnswers = new List<QuestionResponsePair>();
+            var menuCategoryObjects = new List<ParentCategory>();
             var defaultDirectory = Directory.GetCurrentDirectory() + "\\plugins\\BetterPedInteractions\\Default";
             var customDirectory = Directory.GetCurrentDirectory() + "\\plugins\\BetterPedInteractions\\Custom";
             
             LoadXMLs(defaultDirectory);
             LoadXMLs(customDirectory);
-            MenuManager.BuildMenus(questionsAndAnswers);
+            //Game.LogTrivial($"MenuCategory objects: {menuCategoryObjects.Count()}");
+            MenuManager.BuildMenus(menuCategoryObjects);
 
             void LoadXMLs(string directory)
             {
-                foreach (string xmlFile in Directory.GetFiles(directory, "*.xml", SearchOption.AllDirectories))
+                foreach (string file in Directory.GetFiles(directory, "*.xml", SearchOption.AllDirectories))
                 {
-                    //Game.LogTrivial($"File: {xmlFile}");
-                    XDocument document = XDocument.Load(xmlFile);
+                    //Game.LogTrivial($"File: {Path.GetFileNameWithoutExtension(file)}");
+                    XDocument document = XDocument.Load(file);
                     var menu = document.Root.Attribute("menu").Value;
-                    //Game.LogTrivial($"Menu attribute: {menu}");
-                    var questionCategories = document.Descendants("QuestionGroup").Attributes();
-                    var questions = document.Descendants("Question");
-                    var responses = document.Descendants("Response");
+                    //Game.LogTrivial($"Menu: {menu}");
+                    var parsed = Enum.TryParse(menu, out Settings.Group group);
+                    //Game.LogTrivial($"Parsed: {parsed}, Parsed menu: {group}");
+                    var menuCategories = document.Descendants("MenuCategory");
+                    //Game.LogTrivial($"Categories: {menuCategories.Count()}");
 
-                    foreach (XAttribute category in questionCategories)
+                    // For each menu category in the file
+                    foreach(XElement category in menuCategories)
                     {
-                        Game.LogTrivial($"Category: {category.Value}");
-                        if(category.Value == "Ped Actions")
+                        // Create new MenuCategory object
+                        var newMenuCategory = new ParentCategory(category.Element("CategoryName"), group, Path.GetFileNameWithoutExtension(file));
+                        //Game.LogTrivial($"Parent category file: {newMenuCategory.File}");
+                        // Check for submenus
+                        if (category.Elements("SubCategory").Any())
                         {
-                            var questionAnswerPair = new QuestionResponsePair(category);
-                            if (menu == "civilian")
+                            //Game.LogTrivial($"Sub categories: {category.Elements("SubCategory").Count()}");
+                            foreach (XElement categoryType in category.Elements("SubCategory"))
                             {
-                                questionAnswerPair.Group = Settings.Group.Civilian;
+                                var newSubCategory = new SubCategory(categoryType.Element("CategoryName"), newMenuCategory, Path.GetFileNameWithoutExtension(file));
+                                //Game.LogTrivial($"Sub Category file: {newSubCategory.File}");
+                                if(categoryType.Attribute("enableByDefault") != null)
+                                {
+                                    newSubCategory.Enabled = bool.Parse(categoryType.Attribute("enableByDefault").Value);
+                                }
+                                //Game.LogTrivial($"--Sub category: {newSubCategory.Name.Value}");
+                                newSubCategory.MenuItems = CompileMenuItems(newSubCategory, categoryType);
+                                newMenuCategory.SubCategories.Add(newSubCategory);
                             }
-                            else if (menu == "cop")
-                            {
-                                questionAnswerPair.Group = Settings.Group.Cop;
-                            }
-                            questionsAndAnswers.Add(questionAnswerPair);
                         }
-                        foreach (XElement question in questions.Where(x => x.Parent == category.Parent))
+                        else
                         {
-                            var questionAnswerPair = new QuestionResponsePair(category);
-                            if (menu == "civilian")
-                            {
-                                questionAnswerPair.Group = Settings.Group.Civilian;
-                            }
-                            else if (menu == "cop")
-                            {
-                                questionAnswerPair.Group = Settings.Group.Cop;
-                            }
-                            //Game.LogTrivial($"Question: {question.Attribute("question").Value}");
-                            questionAnswerPair.Question = question;
-                            foreach (XElement response in responses.Where(r => r.Parent.Attribute("question").Value == question.Attribute("question").Value))
-                            {
-                                questionAnswerPair.Responses.Add(response);
-                                //Game.LogTrivial($"Response: {response.Value}");
-                            }
-                            questionsAndAnswers.Add(questionAnswerPair);
-                            VocalInterface.Phrases.Add(question.Attribute("question").Value);
+                            newMenuCategory.MenuItems = CompileMenuItems(newMenuCategory, category);
                         }
+                        menuCategoryObjects.Add(newMenuCategory);
                     }
                 }
             }
+        }
+
+        private static List<MenuItem> CompileMenuItems(Category category, XElement categoryType)
+        {
+            IEnumerable<XElement> menuItems;
+            //Game.LogTrivial($"Category Type: {categoryType.Name}");
+            //Game.LogTrivial($"Category Name: {category.Name.Value}");
+            menuItems = categoryType.Elements("MenuItem");
+
+            //Game.LogTrivial($"Menu items: {menuItems.Count()}");
+            foreach (XElement menuItem in menuItems)
+            {
+                var newMenuItem = new MenuItem(menuItem);
+                if(menuItem.Attribute("enableByDefault") != null)
+                {
+                    newMenuItem.Enabled = bool.Parse(menuItem.Attribute("enableByDefault").Value);
+                }
+                newMenuItem.Category = category;
+                if (menuItem.Element("Level") != null)
+                {
+                    newMenuItem.Level = int.Parse(menuItem.Element("Level").Value);
+                }
+                newMenuItem.MenuPrompt = menuItem.Element("MenuPrompt");
+                newMenuItem.Responses = menuItem.Elements("Response").ToList();
+                //Game.LogTrivial($"---Prompt: {newMenuItem.MenuPrompt?.Value}, Level: {newMenuItem.Level}, Responses: {newMenuItem.Responses?.Count()}");
+                category.MenuItems.Add(newMenuItem);
+
+                if(newMenuItem.MenuPrompt != null)
+                {
+                    VocalInterface.Phrases.Add(newMenuItem.MenuPrompt.Value);
+                    //Game.LogTrivial($"Phrase from menu prompt: {newMenuItem.MenuPrompt.Value}");
+                    var audioPrompts = menuItem.Elements("AudioPrompt").Select(x => x.Value).ToList();
+                    foreach (string audioPrompt in audioPrompts)
+                    {
+                        //Game.LogTrivial($"Audio prompt: {audioPrompt}");
+                        VocalInterface.Phrases.Add(audioPrompt);
+                    }
+                }
+            }
+            return category.MenuItems;
         }
     }
 }
