c84ae10fd88bc158fec46ae0702916f28ed79833
Rich Dunne <richdunne@yahoo.com>
2020-12-27 10:32:06 -0700

    Separated MenuItem class from Category class file

---
+++ BetterPedInteractions/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using RAGENativeUI.Elements;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace BetterPedInteractions
+{
+    class MenuItem
+    {
+        internal XElement Element { get; set; }
+        internal Category Category { get; set; }
+        internal bool Enabled { get; set; } = true;
+        internal int Level { get; set; } = 1;
+        internal XElement MenuPrompt { get; set; }
+        internal List<XElement> Responses { get; set; }
+        internal UIMenuItem Action { get; set; }
+
+        internal MenuItem(XElement element)
+        {
+            Element = element;
+        }
+    }
+}
