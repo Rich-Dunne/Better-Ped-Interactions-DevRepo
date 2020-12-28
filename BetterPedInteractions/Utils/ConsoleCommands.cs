2ce4d2c746a2015378e28efcd7a7e2bcd26b2cf7
Rich Dunne <richdunne@yahoo.com>
2020-12-28 09:26:02 -0700

    Initial commit

---
+++ BetterPedInteractions/Utils/ConsoleCommands.cs
@@ -0,0 +1,40 @@
+﻿using Rage;
+using Rage.Attributes;
+using Rage.ConsoleCommands.AutoCompleters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterPedInteractions.Utils
+{
+    class ConsoleCommands
+    {
+        [ConsoleCommand]
+        internal static void Command_EnablePlayerLipAnimation([ConsoleCommandParameter(AutoCompleterType = typeof(ConsoleCommandAutoCompleterBoolean), Name = "EnablePlayerLipAnimation")] bool enabled)
+        {
+            GameFiber.StartNew(() =>
+            {
+                bool PlayerTalking = false;
+                while (enabled)
+                {
+                    GameFiber.Sleep(new Random().Next(1000, 10000));
+                    if (!PlayerTalking)
+                    {
+                        Game.LocalPlayer.Character.Tasks.PlayAnimation("mp_facial", "mic_chatter", 1.0f, AnimationFlags.SecondaryTask);
+                        PlayerTalking = true;
+                    }
+                    GameFiber.Sleep(new Random().Next(1000,10000));
+                    if (PlayerTalking)
+                    {
+                        Game.LocalPlayer.Character.Tasks.ClearSecondary();
+                        PlayerTalking = false;
+                    }
+                    GameFiber.Yield();
+                }
+                Game.LocalPlayer.Character.Tasks.ClearSecondary();
+            }, "Player Lip Animation Command");
+        }
+    }
+}
