eb76db0345127b96d9e9dccf0a508be241c48597
Rich Dunne <richdunne@yahoo.com>
2020-12-31 10:26:31 -0700

    Added properties for ParentCategory, SubCategory, HasSubCategory, and Highlighted.

--- BetterPedInteractions/Objects/MenuItem.cs
+++ BetterPedInteractions/Objects/MenuItem.cs
@@ -7,26 +7,26 @@ namespace BetterPedInteractions
     class MenuItem
     {
         internal XElement Element { get; set; }
-        internal Category Category { get; set; }
+        internal ParentCategory ParentCategory { get; set; }
+        internal SubCategory SubCategory { get; set; }
+        internal bool HasSubCategory { get; private set; } = false;
         internal bool Enabled { get; set; } = true;
         internal int Level { get; set; } = 1;
         internal XElement MenuPrompt { get; set; }
         internal List<XElement> Responses { get; set; } = new List<XElement>();
         internal List<string> AudioPrompts { get; set; } = new List<string>();
         internal UIMenuItem Action { get; set; } = null;
+        internal bool Highlighted { get; set; } = false;
 
-        internal MenuItem(XElement element)
+        internal MenuItem(XElement element, ParentCategory parentCategory, SubCategory subCategory = null)
         {
             Element = element;
-        }
-
-        internal bool BelongsTo(Category category)
-        {
-            if (category.MenuItems.Contains(this))
+            ParentCategory = subCategory != null ? subCategory.ParentCategory : parentCategory;
+            SubCategory = subCategory;
+            if(subCategory != null)
             {
-                return true;
+                HasSubCategory = true;
             }
-            return false;
         }
     }
 }
